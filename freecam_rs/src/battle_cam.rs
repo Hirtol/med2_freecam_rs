ecbecd5d8aa0719a44b9a49e8027273feba7c1b2
Valentijn Hol <git@hirtol.top>
2023-10-29 22:47:23 +0100

    Extract a bunch of the logic into separate methods

--- freecam_rs/src/battle_cam.rs
+++ freecam_rs/src/battle_cam.rs
@@ -28,6 +28,8 @@ pub struct BattleCamState {
     minimal_z: f32,
 }
 
+type Acceleration = Velocity;
+
 #[derive(Default)]
 pub struct Velocity {
     x: f32,
@@ -195,39 +197,14 @@ impl BattleCamState {
         t_delta: Duration,
         conf: &mut FreecamConfig,
     ) -> anyhow::Result<()> {
-        let target_pos = patcher.mut_read(conf.addresses.battle_cam_target_addr.as_mut());
         let camera_pos = patcher.mut_read(conf.addresses.battle_cam_addr.as_mut());
-        let mut acceleration = Velocity::default();
+        let mut acceleration = Acceleration::default();
         let (horizontal_speed, vertical_speed) = calculate_speed_multipliers(conf, key_man);
-
-        let (pitch, yaw) = (self.custom_camera.pitch, self.custom_camera.yaw);
 
         let mut point = POINT::default();
         GetCursorPos(&mut point)?;
 
-        // Detect double click (vanilla functionality retention)
-        if key_man.get_key_state(VK_LBUTTON) == KeyState::Pressed {
-            let now = Instant::now();
-            let time_since_last = now.duration_since(self.last_left_click);
-            self.last_left_click = now;
-
-            println!(
-                "Time since last left: {:#?} and double: {:#?}",
-                time_since_last,
-                GetDoubleClickTime()
-            );
-            println!("Old Cursor: {:#?} - New: {:#?}", self.old_cursor_pos, point);
-
-            if (time_since_last.as_millis() as u32) < GetDoubleClickTime()
-                && (self.old_cursor_pos.x - point.x).abs() < 10
-                && (self.old_cursor_pos.y - point.y).abs() < 10
-            {
-                println!("Pausing!");
-                self.is_moving_toward_unit = true;
-                self.pause(true, patcher);
-            }
-        }
-
+        // If some external source modified it with our consent we should probably update our camera.
         if (self.custom_camera.x - camera_pos.x_coord).abs() > f32::EPSILON
             || (self.custom_camera.y - camera_pos.y_coord).abs() > f32::EPSILON
             || (self.custom_camera.z - camera_pos.z_coord).abs() > f32::EPSILON
@@ -235,7 +212,81 @@ impl BattleCamState {
             self.sync_custom_camera(patcher, conf);
         }
 
+        // Handle scroll
+        self.bc_handle_scroll(scroll, conf, vertical_speed);
+
+        // Detect double click (vanilla functionality retention)
+        self.bc_handle_left_click(patcher, key_man, point);
+
         // Adjust based on free-cam movement
+        self.bc_handle_panning(patcher, key_man, conf, &mut acceleration, &mut point);
+
+        // Camera movement
+        self.bc_move_camera(patcher, key_man, conf, &mut acceleration);
+
+        // Rotation controls
+        self.bc_handle_rotation(patcher, key_man, conf, &mut acceleration);
+
+        // Update velocity based on the new `acceleration`
+        self.bc_calculate_current_velocity(conf, &acceleration, horizontal_speed, vertical_speed);
+
+        self.custom_camera.x += self.velocity.x;
+        self.custom_camera.y += self.velocity.y;
+        self.custom_camera.z += self.velocity.z;
+        self.custom_camera.pitch += self.velocity.pitch;
+        self.custom_camera.yaw += self.velocity.yaw;
+
+        self.bc_smooth_decay_velocity(conf);
+
+        self.bc_restrict_coordinates(conf);
+
+        if !self.paused {
+            // Important that this runs _before_ pitch/yaw adjustment as they're dependent.
+            write_custom_camera(&self.custom_camera, camera_pos);
+
+            let target_pos = patcher.mut_read(conf.addresses.battle_cam_target_addr.as_mut());
+            write_pitch_yaw(camera_pos, target_pos, self.custom_camera.pitch, self.custom_camera.yaw);
+        } else {
+            // Update our custom camera values.
+            self.sync_custom_camera(patcher, conf);
+        }
+
+        // Persist info for next loop
+        self.old_cursor_pos = point;
+        Ok(())
+    }
+
+    unsafe fn bc_handle_left_click(&mut self, patcher: &mut LocalPatcher, key_man: &mut KeyboardManager, point: POINT) {
+        if key_man.get_key_state(VK_LBUTTON) == KeyState::Pressed {
+            let now = Instant::now();
+            let time_since_last = now.duration_since(self.last_left_click);
+            self.last_left_click = now;
+
+            if (time_since_last.as_millis() as u32) < GetDoubleClickTime()
+                && (self.old_cursor_pos.x - point.x).abs() < 10
+                && (self.old_cursor_pos.y - point.y).abs() < 10
+            {
+                self.is_moving_toward_unit = true;
+                self.pause(true, patcher);
+            }
+        }
+    }
+
+    fn bc_handle_scroll(&mut self, scroll: &mut ScrollTracker, conf: &FreecamConfig, vertical_speed: f32) {
+        // TODO: Figure out how this works.
+        let scroll_delta = scroll.get_scroll_delta() * if conf.camera.inverted_scroll { -1 } else { 1 };
+        let is_negative = if scroll_delta != 0 { scroll_delta.abs() / scroll_delta } else { 1 };
+        self.velocity.z += (scroll_delta.pow(2) * is_negative) as f32 * vertical_speed / 10.;
+    }
+
+    unsafe fn bc_handle_panning(
+        &mut self,
+        patcher: &mut LocalPatcher,
+        key_man: &mut KeyboardManager,
+        conf: &mut FreecamConfig,
+        acceleration: &mut Velocity,
+        point: &mut POINT,
+    ) {
         if key_man.has_pressed(VIRTUAL_KEY(conf.keybinds.freecam_key)) {
             let invert = if conf.camera.inverted { -1.0 } else { 1.0 };
             let adjusted_sens = conf.camera.sensitivity * (1. - conf.camera.pan_smoothing);
@@ -244,8 +295,34 @@ impl BattleCamState {
             // We should have control again.
             self.pause(false, patcher);
         }
+    }
 
-        // Camera movement
+    unsafe fn bc_handle_rotation(
+        &mut self,
+        patcher: &mut LocalPatcher,
+        key_man: &mut KeyboardManager,
+        conf: &mut FreecamConfig,
+        acceleration: &mut Velocity,
+    ) {
+        let pan_speed = 1. - conf.camera.pan_smoothing;
+        if key_man.has_pressed(VIRTUAL_KEY(conf.keybinds.rotate_left)) {
+            acceleration.yaw += 0.03 * pan_speed;
+            self.pause(false, patcher);
+        }
+        if key_man.has_pressed(VIRTUAL_KEY(conf.keybinds.rotate_right)) {
+            acceleration.yaw -= 0.03 * pan_speed;
+            self.pause(false, patcher);
+        }
+    }
+
+    unsafe fn bc_move_camera(
+        &mut self,
+        patcher: &mut LocalPatcher,
+        key_man: &mut KeyboardManager,
+        conf: &FreecamConfig,
+        acceleration: &mut Velocity,
+    ) {
+        let yaw = self.custom_camera.yaw;
         if key_man.has_pressed(VIRTUAL_KEY(conf.keybinds.forward_key)) {
             acceleration.y += yaw.sin();
             acceleration.x += yaw.cos();
@@ -266,44 +343,11 @@ impl BattleCamState {
             acceleration.x += ((3. * PI / 2.) + yaw).cos();
             self.pause(false, patcher);
         }
+    }
 
-        // Rotation controls
-        let pan_speed = 1. - conf.camera.pan_smoothing;
-        if key_man.has_pressed(VIRTUAL_KEY(conf.keybinds.rotate_left)) {
-            acceleration.yaw += 0.03 * pan_speed;
-            self.pause(false, patcher);
-        }
-        if key_man.has_pressed(VIRTUAL_KEY(conf.keybinds.rotate_right)) {
-            acceleration.yaw -= 0.03 * pan_speed;
-            self.pause(false, patcher);
-        }
-
-        // Handle scroll TODO: Figure out how this works.
-        let scroll_delta = scroll.get_scroll_delta() * if conf.camera.inverted_scroll { -1 } else { 1 };
-        let did_scroll = scroll_delta != 0;
-        let is_negative = if scroll_delta != 0 { scroll_delta.abs() / scroll_delta } else { 1 };
-        self.velocity.z += (scroll_delta.pow(2) * is_negative) as f32 * vertical_speed / 10.;
-
-        // Write the new camera values
-        let mut length = (acceleration.x.powi(2) + acceleration.y.powi(2) + acceleration.z.powi(2)).sqrt();
-        if length == 0. {
-            length = 1.;
-        }
-
-        self.velocity.x +=
-            ((acceleration.x / length) * (horizontal_speed * (1. - conf.camera.horizontal_smoothing))) / 2.;
-        self.velocity.y +=
-            ((acceleration.y / length) * (horizontal_speed * (1. - conf.camera.horizontal_smoothing))) / 2.;
-        self.velocity.z += ((acceleration.z / length) * (vertical_speed * (1. - conf.camera.vertical_smoothing))) / 2.;
-        // Freecam
-        self.velocity.pitch += acceleration.pitch;
-        self.velocity.yaw += acceleration.yaw;
-
-        self.custom_camera.x += self.velocity.x;
-        self.custom_camera.y += self.velocity.y;
-        self.custom_camera.z += self.velocity.z;
-        self.custom_camera.pitch += self.velocity.pitch;
-        self.custom_camera.yaw += self.velocity.yaw;
+    fn bc_restrict_coordinates(&mut self, conf: &mut FreecamConfig) {
+        self.custom_camera.x = 900.0f32.min((-900.0f32).max(self.custom_camera.x));
+        self.custom_camera.y = 900.0f32.min((-900.0f32).max(self.custom_camera.y));
 
         if conf.camera.maintain_relative_height {
             let new_z_diff = self.custom_camera.z - f32::from_bits(self.remote_z_max.load(Ordering::SeqCst));
@@ -342,39 +386,37 @@ impl BattleCamState {
                 self.custom_camera.z = (self.minimal_z + (multiplier * 2.1)).max(self.custom_camera.z);
             }
         }
+    }
 
+    unsafe fn bc_calculate_current_velocity(
+        &mut self,
+        conf: &FreecamConfig,
+        acceleration: &Acceleration,
+        horizontal_speed: f32,
+        vertical_speed: f32,
+    ) {
+        let mut length = (acceleration.x.powi(2) + acceleration.y.powi(2) + acceleration.z.powi(2)).sqrt();
+
+        if length == 0. {
+            length = 1.;
+        }
+
+        self.velocity.x +=
+            ((acceleration.x / length) * (horizontal_speed * (1. - conf.camera.horizontal_smoothing))) / 2.;
+        self.velocity.y +=
+            ((acceleration.y / length) * (horizontal_speed * (1. - conf.camera.horizontal_smoothing))) / 2.;
+        self.velocity.z += ((acceleration.z / length) * (vertical_speed * (1. - conf.camera.vertical_smoothing))) / 2.;
+        // Freecam
+        self.velocity.pitch += acceleration.pitch;
+        self.velocity.yaw += acceleration.yaw;
+    }
+
+    fn bc_smooth_decay_velocity(&mut self, conf: &FreecamConfig) {
         self.velocity.x *= conf.camera.horizontal_smoothing;
         self.velocity.y *= conf.camera.horizontal_smoothing;
         self.velocity.z *= conf.camera.vertical_smoothing;
         self.velocity.pitch *= conf.camera.pan_smoothing;
         self.velocity.yaw *= conf.camera.pan_smoothing;
-
-        // println!(
-        //     "In battle! {:#?} - {:#?}",
-        //     patcher.read(conf.addresses.battle_cam_addr.as_ref()),
-        //     patcher.read(conf.addresses.battle_cam_target_addr.as_ref())
-        // );
-        //
-        // println!("Pitch: {} - Yaw: {}", pitch, yaw);
-
-        println!("Raw Z: {}", f32::from_bits(self.remote_z_max.load(Ordering::SeqCst)));
-
-        self.custom_camera.x = 900.0f32.min((-900.0f32).max(self.custom_camera.x));
-        self.custom_camera.y = 900.0f32.min((-900.0f32).max(self.custom_camera.y));
-
-        // Write to the addresses
-        if !self.paused {
-            // Important that this runs _before_ pitch/yaw adjustment as they're dependent.
-            write_custom_camera(&self.custom_camera, camera_pos);
-            write_pitch_yaw(camera_pos, target_pos, pitch, yaw);
-        } else {
-            // Update
-            self.sync_custom_camera(patcher, conf);
-        }
-
-        // Persist info for next loop
-        self.old_cursor_pos = point;
-        Ok(())
     }
 
     unsafe fn pause(&mut self, paused: bool, patcher: &mut LocalPatcher) {
@@ -406,12 +448,7 @@ impl BattleCamState {
     }
 }
 
-fn write_pitch_yaw(
-    camera_pos: &mut BattleCameraView,
-    target_pos: &mut BattleCameraTargetView,
-    mut pitch: f32,
-    yaw: f32,
-) {
+fn write_pitch_yaw(camera_pos: &BattleCameraView, target_pos: &mut BattleCameraTargetView, mut pitch: f32, yaw: f32) {
     pitch = pitch.max(-(PI / 2.) * 0.9);
     pitch = pitch.min((PI / 2.) * 0.9);
 
